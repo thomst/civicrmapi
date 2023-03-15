e8b1f3acaaa58491bea75d97acaf13c723798a89
Thomas Leichtfuß <leichtfuss@systopia.de>
2023-03-15 21:43:41 +0100

    added some simple tests

---
+++ civicrmapi/tests.py
@@ -0,0 +1,42 @@
+import unittest
+from . import v3, v4
+from .api import BaseApi
+from .api import BaseEntity
+from .api import RestApiV3
+from .api import RestApiV4
+
+
+class TestApiConstruction(unittest.TestCase):
+
+    def setUp(self):
+        self.url = 'http://fixme.de'
+        self.api_key = 'fixme'
+        self.site_key = 'fixme'
+        self.base_api_v3 = type('api', (BaseApi,), dict(VERSION=v3))
+        self.base_api_v4 = type('api', (BaseApi,), dict(VERSION=v4))
+
+    def test_api_initialization(self):
+        api = self.base_api_v3()
+        for entity in v3.ENTITIES:
+            self.assertTrue(hasattr(api, entity))
+            for action in v3.ACTIONS:
+                self.assertTrue(hasattr(getattr(api, entity), action))
+
+        api = self.base_api_v4()
+        for entity in v4.ENTITIES:
+            self.assertTrue(hasattr(api, entity))
+            for action in v4.ACTIONS:
+                self.assertTrue(hasattr(getattr(api, entity), action))
+
+    def test_entity_initialization(self):
+        api = self.base_api_v4()
+        # Setup an entity class with a get attribute.
+        entity_class = type('MyEntity', (BaseEntity,), dict(get='foobar'))
+        entity = entity_class(api)
+
+        # Check that default actions were added to the entity.
+        for action in v4.ACTIONS:
+            self.assertTrue(hasattr(entity, action))
+
+        # Check that the get-attribute was not overwritten.
+        self.assertEqual(entity.get, 'foobar')
