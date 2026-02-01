6b6c9c27fb9925447277d452cc7acc8806961647
Thomas Leichtfuß <leichtfuss@systopia.de>
2026-02-01 21:59:41 +0100

    added debugging statements in install-docker-civicrm script

--- install-docker-civicrm.sh
+++ install-docker-civicrm.sh
@@ -10,28 +10,35 @@ CIVICRM_VARS_JSON_FILE='civicrm-vars-data.json'
 
 # Clone civicrm-docker and jump into the example civicrm folder.
 if [ ! -d civicrm-docker ]; then
+    echo -e "\nCloning civicrm-docker repository..."
     git clone https://github.com/civicrm/civicrm-docker || exit
 fi
 
 # Navigate to example civicrm directory.
+echo -e "\nJumping into civicrm-docker/example/civicrm folder..."
 cd civicrm-docker/example/civicrm || exit
 
 # Create env file with mysql credentials.
+echo -e "\nCreating .env file with mysql credentials..."
 cat >.env <<EOL
 MYSQL_PASSWORD=${MYSQL_PASSWORD}
 MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}
 EOL
 
 # Setup docker container.
+echo -e "\nStarting docker containers..."
 docker-compose up --detach || exit
 
 # Wait for mariadb to be ready.
+echo -e "\nWaiting for mariadb to be ready..."
 while ! docker-compose logs | grep -q "socket: '/run/mysqld/mysqld.sock'  port: 3306"; do
     sleep 1
 done
 
 # Check if the settings file already exists. Otherwise install civicrm.
+echo -e "\nChecking if CiviCRM is already installed..."
 if ! docker-compose exec app test -f ./private/civicrm.settings.php; then
+    echo -e "\nCiviCRM not installed. Installing CiviCRM..."
     docker-compose exec \
         -u www-data \
         -e CIVICRM_ADMIN_USER="$CIVICRM_ADMIN_USER" \
@@ -40,6 +47,7 @@ if ! docker-compose exec app test -f ./private/civicrm.settings.php; then
 fi
 
 # Set api key for admin user.
+echo -e "\nSetting API key for admin user and writing to json file..."
 docker-compose exec app \
     cv api4 Contact.update \
     +v "api_key=$CIVICRM_ADMIN_API_KEY" \
@@ -47,6 +55,7 @@ docker-compose exec app \
     > "$CIVICRM_ADMIN_JSON_FILE" || exit
 
 # Write civicrm vars to json-config-file.
+echo -e "\nWriting CiviCRM vars to json file..."
 docker-compose exec -u www-data app \
     cv vars:show --out=json-pretty \
     > "$CIVICRM_VARS_JSON_FILE" || exit
