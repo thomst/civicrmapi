4b127bddd7cacd04be25d8b4311db9624ad93bc1
Thomas Leichtfuß <leichtfuss@systopia.de>
2026-02-01 15:45:34 +0100

    fixed tests for docker test installation

--- tests.py
+++ tests.py
@@ -1,26 +1,47 @@
+import json
 import os
 import unittest
 import logging
+from pathlib import Path
 from civicrmapi import __version__
 from civicrmapi import v3, v4
 from civicrmapi.errors import ApiError
-from civicrmapi.errors import InvokeError
+from civicrmapi.errors import SubprocessError
 from civicrmapi.base import BaseApi
-from civicrmapi.base import BaseEntity
 from civicrmapi.rest import RestApiV3
 from civicrmapi.rest import RestApiV4
 from civicrmapi.console import ConsoleApiV3
 from civicrmapi.console import ConsoleApiV4
 
 
-URL = os.environ.get('APITEST_URL', None)
-API_KEY = os.environ.get('APITEST_API_KEY', None)
-SITE_KEY = os.environ.get('APITEST_SITE_KEY', None)
-CV = os.environ.get('APITEST_CV', None)
-CWD = os.environ.get('APITEST_CWD', None)
-CONTEXT = os.environ.get('APITEST_CONTEXT', None)
-LOG_LEVEL = getattr(logging, os.environ.get('APITEST_LOG_LEVEL', 'warning').upper())
+# Get directory of this file and build paths to json config files.
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+PATH_TO_CIVICRM_DOCKER = Path(THIS_DIR) / 'civicrm-docker/example/civicrm'
+CIVICRM_ADMIN_JSON_FILE = Path(PATH_TO_CIVICRM_DOCKER) / 'civicrm-admin-data.json'
+CIVICRM_VARS_JSON_FILE = Path(PATH_TO_CIVICRM_DOCKER) / 'civicrm-vars-data.json'
 
+# Setup some defaults to work with the api of the CiviCRM test installation.
+CV = 'cv'
+CONTEXT = 'docker-compose exec app bash -c'
+
+# Get relevant config data from json files if they exist.
+CIVI_CONFIG = dict()
+if CIVICRM_ADMIN_JSON_FILE.is_file() and CIVICRM_VARS_JSON_FILE.is_file():
+    with open(CIVICRM_ADMIN_JSON_FILE, 'r') as f:
+        data = json.load(f)
+        CIVI_CONFIG['api_key'] = data[0]['api_key']
+    with open(CIVICRM_VARS_JSON_FILE, 'r') as f:
+        data = json.load(f)
+        CIVI_CONFIG['url'] = data['CMS_URL']
+        CIVI_CONFIG['site_key'] = data['CIVI_SITE_KEY']
+
+    # To run cv commands within the docker container we need to jump into the
+    # civicrm docker root path.
+    os.chdir(PATH_TO_CIVICRM_DOCKER)
+
+
+# Setup the logger.
+LOG_LEVEL = getattr(logging, os.environ.get('APITEST_LOG_LEVEL', 'warning').upper())
 logging.basicConfig(level=LOG_LEVEL)
 
 
@@ -76,34 +97,34 @@ class ApiTestCase(unittest.TestCase):
         api = ConsoleApiV4('dummy_cv', 'dummy_cwd')
         self.assertRaises(Exception, api.Contact.get, ['more', 'arguments'])
 
-    @needs(URL, API_KEY)
+    @needs(CIVI_CONFIG)
     def test_rest_api_v4_call(self):
-        api = RestApiV4(URL, API_KEY)
+        api = RestApiV4(CIVI_CONFIG['url'], CIVI_CONFIG['api_key'])
         result = api.Contact.get()
         self.assertIsInstance(result, list)
 
-    @needs(URL, API_KEY, SITE_KEY)
+    @needs(CIVI_CONFIG)
     def test_rest_api_v3_call(self):
-        api = RestApiV3(URL, API_KEY, SITE_KEY)
+        api = RestApiV3(CIVI_CONFIG['url'], CIVI_CONFIG['api_key'], CIVI_CONFIG['site_key'])
         result = api.Contact.get()
         self.assertIsInstance(result, list)
 
-    @needs(CV, CWD)
+    @needs(CIVI_CONFIG)
     def test_console_api_call(self):
-        api = ConsoleApiV3(CV, CWD, CONTEXT)
+        api = ConsoleApiV3(CV, context=CONTEXT)
         result = api.Contact.get()
         self.assertIsInstance(result, list)
 
-        api = ConsoleApiV4(CV, CWD, CONTEXT)
+        api = ConsoleApiV4(CV, context=CONTEXT)
         result = api.Contact.get()
         self.assertIsInstance(result, list)
 
-    @needs(URL, API_KEY, SITE_KEY, CV, CWD)
+    @needs(CIVI_CONFIG)
     def test_compare_rest_and_console_results(self):
-        api = RestApiV4(URL, API_KEY)
+        api = RestApiV4(CIVI_CONFIG['url'], CIVI_CONFIG['api_key'])
         rest_result = api.Contact.get()
         self.assertIsInstance(rest_result, list)
-        api = ConsoleApiV4(CV, CWD, CONTEXT)
+        api = ConsoleApiV4(CV, context=CONTEXT)
         console_result = api.Contact.get()
         self.assertIsInstance(console_result, list)
         self.assertEqual(rest_result, console_result)
@@ -113,74 +134,74 @@ class ApiTestCase(unittest.TestCase):
             where=[['contact_type', '=', 'Organization']],
             limit=1
             )
-        api = RestApiV4(URL, API_KEY)
+        api = RestApiV4(CIVI_CONFIG['url'], CIVI_CONFIG['api_key'])
         rest_result = api.Contact.get(params)
         self.assertIsInstance(rest_result, list)
-        api = ConsoleApiV4(CV, CWD, CONTEXT)
+        api = ConsoleApiV4(CV, context=CONTEXT)
         console_result = api.Contact.get(params)
         self.assertIsInstance(console_result, list)
         self.assertEqual(rest_result, console_result)
 
-        api = RestApiV3(URL, API_KEY, SITE_KEY)
+        api = RestApiV3(CIVI_CONFIG['url'], CIVI_CONFIG['api_key'], CIVI_CONFIG['site_key'])
         rest_result = api.Contact.get()
         self.assertIsInstance(rest_result, list)
-        api = ConsoleApiV3(CV, CWD, CONTEXT)
+        api = ConsoleApiV3(CV, context=CONTEXT)
         console_result = api.Contact.get()
         self.assertIsInstance(console_result, list)
         self.assertEqual(rest_result, console_result)
 
         params = {'return': 'id,contact_type', 'contact_type': 'Organization'}
-        api = RestApiV3(URL, API_KEY, SITE_KEY)
+        api = RestApiV3(CIVI_CONFIG['url'], CIVI_CONFIG['api_key'], CIVI_CONFIG['site_key'])
         rest_result = api.Contact.get(params)
         self.assertIsInstance(rest_result, list)
-        api = ConsoleApiV3(CV, CWD, CONTEXT)
+        api = ConsoleApiV3(CV, context=CONTEXT)
         console_result = api.Contact.get(params)
         self.assertIsInstance(console_result, list)
         self.assertEqual(rest_result, console_result)
 
-    @needs(URL, API_KEY, SITE_KEY, CV, CWD)
+    @needs(CIVI_CONFIG)
     def test_invalid_api_calls(self):
         # Invalid api calls.
-        api = RestApiV4(URL, API_KEY)
+        api = RestApiV4(CIVI_CONFIG['url'], CIVI_CONFIG['api_key'])
         self.assertRaises(ApiError, api.Contact, 'foobar')
-        api = RestApiV3(URL, API_KEY, SITE_KEY)
+        api = RestApiV3(CIVI_CONFIG['url'], CIVI_CONFIG['api_key'], CIVI_CONFIG['site_key'])
         self.assertRaises(ApiError, api.Contact, 'foobar')
-        api = ConsoleApiV4(CV, CWD, CONTEXT)
+        api = ConsoleApiV4(CV, context=CONTEXT)
         self.assertRaises(ApiError, api.Contact, 'foobar')
-        api = ConsoleApiV3(CV, CWD, CONTEXT)
+        api = ConsoleApiV3(CV, context=CONTEXT)
         self.assertRaises(ApiError, api.Contact, 'foobar')
 
         # Invalid credentials.
-        api = RestApiV4(URL, 'FAKE_API_KEY')
+        api = RestApiV4(CIVI_CONFIG['url'], 'FAKE_API_KEY')
         self.assertRaises(ApiError, api.Contact.get)
-        api = RestApiV3(URL, 'FAKE_API_KEY', 'FAKE_SITE_KEY')
+        api = RestApiV3(CIVI_CONFIG['url'], 'FAKE_API_KEY', 'FAKE_SITE_KEY')
         self.assertRaises(ApiError, api.Contact.get)
 
         # Invalid cv command.
-        api = ConsoleApiV4('FAKE_CV', CWD)
+        api = ConsoleApiV4('FAKE_CV', context=CONTEXT)
         self.assertRaises(ApiError, api.Contact.get)
-        api = ConsoleApiV3('FAKE_CV', CWD)
+        api = ConsoleApiV3('FAKE_CV', context=CONTEXT)
         self.assertRaises(ApiError, api.Contact.get)
 
-    @needs(URL)
+    @needs(CIVI_CONFIG)
     def test_render_api_errors(self):
         try:
-            RestApiV4(URL, 'FAKE_API_KEY').Contact.get()
+            RestApiV4(CIVI_CONFIG['url'], 'FAKE_API_KEY').Contact.get()
         except ApiError as exc:
             self.assertTrue('HTTP-CODE' in str(exc))
         try:
-            RestApiV3(URL, 'FAKE_API_KEY', 'FAKE_SITE_KEY').Contact.get()
+            RestApiV3(CIVI_CONFIG['url'], 'FAKE_API_KEY', 'FAKE_SITE_KEY').Contact.get()
         except ApiError as exc:
             self.assertTrue('Invalid credential' in str(exc))
         try:
-            ConsoleApiV4(CV, CWD, CONTEXT).Contact('foobar')
-        except InvokeError:
+            ConsoleApiV4(CV, context=CONTEXT).Contact('foobar')
+        except SubprocessError:
             pass
         except ApiError as exc:
             self.assertTrue('version 4 does not exist' in str(exc))
         try:
-            ConsoleApiV3(CV, CWD, CONTEXT).Contact('foobar')
-        except InvokeError:
+            ConsoleApiV3(CV, context=CONTEXT).Contact('foobar')
+        except SubprocessError:
             pass
         except ApiError as exc:
             self.assertTrue('not-found' in str(exc))
