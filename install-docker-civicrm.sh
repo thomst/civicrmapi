5ce2a264f0d535005dee3eb98d50fe9e1225e41e
Thomas Leichtfuß <leichtfuss@systopia.de>
2026-02-01 22:04:47 +0100

    removed newline prefix in debug statements

--- install-docker-civicrm.sh
+++ install-docker-civicrm.sh
@@ -10,33 +10,33 @@ CIVICRM_VARS_JSON_FILE='civicrm-vars-data.json'
 
 # Clone civicrm-docker and jump into the example civicrm folder.
 if [ ! -d civicrm-docker ]; then
-    echo -e "\nCloning civicrm-docker repository..."
+    echo "Cloning civicrm-docker repository..."
     git clone https://github.com/civicrm/civicrm-docker || exit
 fi
 
 # Navigate to example civicrm directory.
-echo -e "\nJumping into civicrm-docker/example/civicrm folder..."
+echo "Jumping into civicrm-docker/example/civicrm folder..."
 cd civicrm-docker/example/civicrm || exit
 
 # Create env file with mysql credentials.
-echo -e "\nCreating .env file with mysql credentials..."
+echo "Creating .env file with mysql credentials..."
 cat >.env <<EOL
 MYSQL_PASSWORD=${MYSQL_PASSWORD}
 MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}
 EOL
 
 # Setup docker container.
-echo -e "\nStarting docker containers..."
+echo "Starting docker containers..."
 docker-compose up --detach || exit
 
 # Wait for mariadb to be ready.
-echo -e "\nWaiting for mariadb to be ready..."
+echo "Waiting for mariadb to be ready..."
 while ! docker-compose logs | grep -q "socket: '/run/mysqld/mysqld.sock'  port: 3306"; do
     sleep 1
 done
 
 # Check if the settings file already exists. Otherwise install civicrm.
-echo -e "\nChecking if CiviCRM is already installed..."
+echo "Checking if CiviCRM is already installed..."
 if ! docker-compose exec app test -f ./private/civicrm.settings.php; then
     echo -e "\nCiviCRM not installed. Installing CiviCRM..."
     docker-compose exec \
@@ -47,7 +47,7 @@ if ! docker-compose exec app test -f ./private/civicrm.settings.php; then
 fi
 
 # Set api key for admin user.
-echo -e "\nSetting API key for admin user and writing to json file..."
+echo "Setting API key for admin user and writing to json file..."
 docker-compose exec app \
     cv api4 Contact.update \
     +v "api_key=$CIVICRM_ADMIN_API_KEY" \
@@ -55,7 +55,7 @@ docker-compose exec app \
     > "$CIVICRM_ADMIN_JSON_FILE" || exit
 
 # Write civicrm vars to json-config-file.
-echo -e "\nWriting CiviCRM vars to json file..."
+echo "Writing CiviCRM vars to json file..."
 docker-compose exec -u www-data app \
     cv vars:show --out=json-pretty \
     > "$CIVICRM_VARS_JSON_FILE" || exit
